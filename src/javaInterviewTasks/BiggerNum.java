314dc1d99054f84da4050b50e33e6f9f5ea8bef1
13464 <daurentk@gmail.com>
2022-12-16 16:24:10 -0600

    Java Task BiggerNum is added

---
+++ src/javaInterviewTasks/BiggerNum.java
@@ -0,0 +1,17 @@
+package javaInterviewTasks;
+
+public class BiggerNum {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+Write a program that checks for the bigger of 3 numbers.
+        you get 3 int variables with DIFFERENT values: n1 , n2 and n3
+
+            if n1 is biggest output: ""n1 is bigger""
+            if n2 is biggest output: ""n2 is bigger""
+            if n3 is biggest output: ""n3 is bigger""
+
+         NOTE: MUST USE TERNARY"
+ */
