6c9a566a9b7a2ef1ba90be8d90ce3f75df2e8b29
13464 <daurentk@gmail.com>
2022-12-16 16:33:57 -0600

    WithoutFirstChar is added

---
+++ src/javaInterviewTasks/WithoutFirstChar.java
@@ -0,0 +1,17 @@
+package javaInterviewTasks;
+
+public class WithoutFirstChar {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+Create a class named WithoutFirstChar. Ask user to enter two words.
+Print first word without its first character
+    then print the second word without its first character.
+            Input:
+                apple
+                banana
+            Output:
+                ppleanana"
+ */
